b8b0f30a924f308d23c4c20d6499b09b9e1cdc92
Masato Minda <belgianbeer@aj.admwt.jp>
2019-05-16 10:04:44 +0900

    update from 1.1 to 1.1.1 for bugfix when using -HH

--- Formula/tzdiff.rb
+++ Formula/tzdiff.rb
@@ -1,8 +1,8 @@
 class Tzdiff < Formula
   desc "Displays Timezone differences with localtime in CLI (shell script)"
   homepage "https://github.com/belgianbeer/tzdiff"
-  url "https://github.com/belgianbeer/tzdiff/archive/1.1.tar.gz"
-  sha256 "3d2f6bfb9aad3843f198e8e4ba074209287f92b5425b753aa7582b85b3bb5103"
+  url "https://github.com/belgianbeer/tzdiff/archive/1.1.1.tar.gz"
+  sha256 "e1274a472e16823f5defaf3b908f3cb73b314c46bbb22f019bd8e4b7b318c992"
 
   def install
     bin.install "tzdiff"
