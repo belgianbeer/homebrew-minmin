e473399f0137d98c9e30b2889af3a03b013fa654
Masato Minda <belgianbeer@aj.admwt.jp>
2023-07-19 17:02:36 +0900

    for tzdiff 1.2

--- Formula/tzdiff.rb
+++ Formula/tzdiff.rb
@@ -3,7 +3,7 @@ class Tzdiff < Formula
   homepage "https://github.com/belgianbeer/tzdiff"
   url "https://github.com/belgianbeer/tzdiff/archive/1.2.tar.gz"
   sha256 "345e1b6ae8f91b7e03fa47a959936bafd052723c88e152ec56130c138c7b5e85"
-  liscense "BSD-2-Clause"
+  license "BSD-2-Clause"
 
   def install
     bin.install "tzdiff"
