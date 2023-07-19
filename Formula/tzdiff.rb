3c6968b0f7ae84bc8f4916736d447ad9c32aeb0f
Masato Minda <belgianbeer@aj.admwt.jp>
2023-07-19 17:01:07 +0900

    for tzdiff 1.2

--- Formula/tzdiff.rb
+++ Formula/tzdiff.rb
@@ -1,8 +1,9 @@
 class Tzdiff < Formula
   desc "Displays Timezone differences with localtime in CLI (shell script)"
   homepage "https://github.com/belgianbeer/tzdiff"
-  url "https://github.com/belgianbeer/tzdiff/archive/1.1.1.tar.gz"
-  sha256 "e1274a472e16823f5defaf3b908f3cb73b314c46bbb22f019bd8e4b7b318c992"
+  url "https://github.com/belgianbeer/tzdiff/archive/1.2.tar.gz"
+  sha256 "345e1b6ae8f91b7e03fa47a959936bafd052723c88e152ec56130c138c7b5e85"
+  liscense "BSD-2-Clause"
 
   def install
     bin.install "tzdiff"
